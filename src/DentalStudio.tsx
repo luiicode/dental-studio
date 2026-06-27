aaaf0dabea41b7bd5284f4600e83fc0c8c26432f
luiicode <luiseduardo.corte35@gmail.com>
2026-06-27 01:41:05 -0600

    feat: implement DentalStudio component and update main render

---
+++ src/DentalStudio.tsx
@@ -0,0 +1,5 @@
+export function DentalStudio() {
+    return (
+        <h1>Hello, React!</h1>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 
+import {DentalStudio} from "@/DentalStudio.tsx";
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <h1>Hello, React!</h1>
+        <DentalStudio/>
     </StrictMode>,
 )
