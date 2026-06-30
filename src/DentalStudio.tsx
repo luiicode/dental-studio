796823be97b8fa39d700cb1a056c87d5a7fb4d53
luiicode <luiseduardo.corte35@gmail.com>
2026-06-30 14:14:30 -0600

    feat(layout): add main landing page structure

--- src/DentalStudio.tsx
+++ src/DentalStudio.tsx
@@ -1,5 +1,21 @@
+import {Navbar} from "@/components/layout/Navbar.tsx";
+import {Hero} from "@/components/section/Hero.tsx";
+import {Services} from "@/components/section/Services.tsx";
+import {Testimonials} from "@/components/section/Testimonials.tsx";
+import {Footer} from "@/components/layout/Footer.tsx";
+
 export function DentalStudio() {
     return (
-        <h1>Hello, React!</h1>
+        <div
+            className='bg-background-primary min-h-screen w-full pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] box-border'>
+            <section
+                className='flex flex-col flex-1 gap-5 w-full'>
+                <Navbar/>
+                <Hero/>
+                <Services/>
+                <Testimonials/>
+                <Footer/>
+            </section>
+        </div>
     );
 }
